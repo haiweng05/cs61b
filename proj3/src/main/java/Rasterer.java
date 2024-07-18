bd36e7498ab3a9c788d2f7990962eb9e1b3ac16e
haiweng05 <seamanx05@gmail.com>
2024-07-18 16:50:01 +0800

    proj3 phase1 finished

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -44,9 +44,102 @@ public class Rasterer {
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
         // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+
+        double ullat = params.get("ullat");
+        double ullon = params.get("ullon");
+        double lrlat = params.get("lrlat");
+        double lrlon = params.get("lrlon");
+        double w = params.get("w");
+        double h = params.get("h");
+        if (w < 0 || h < 0 || !check(ullon, ullat, lrlon, lrlat)) {
+            results.put("query_success", false);
+            return results;
+        }
+
+
+        /* Consider the lonDpp and latDpp of the tile */
+        double basicLonDpp = -(MapServer.ROOT_ULLON - MapServer.ROOT_LRLON) / MapServer.TILE_SIZE;
+        double basicLatDpp = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / MapServer.TILE_SIZE;
+
+        double queryLonDpp = -(ullon - lrlon) / w;
+        double queryLatDpp = (ullat - lrlat) / h;
+
+        int times = 64;
+        int depth = 7;
+
+
+        while (basicLonDpp / times <= queryLonDpp && basicLatDpp / times <= queryLatDpp && times >= 1) {
+            depth -= 1;
+            times /= 2;
+        }
+        times *= 2;
+        results.put("depth", depth);
+//
+//        int nY = (int) Math.ceil(h / MapServer.TILE_SIZE) + 1;
+//        int nX = (int) Math.ceil(w / MapServer.TILE_SIZE) + 1;
+//
+//        /* The minimal lonDpp and latDpp */
+//        double lonDpp = -(ullon - lrlon) / (nX * MapServer.TILE_SIZE);
+//        double latDpp = (ullat - lrlat) / (nY * MapServer.TILE_SIZE);
+//
+//
+//        int times = 64;
+//        int depth = 7;
+        double curLonDpp = basicLonDpp / times;
+        double curLatDpp = basicLatDpp / times;
+
+        double numPixelX = (ullon - MapServer.ROOT_ULLON) / curLonDpp;
+        int startX = (int) Math.floor(numPixelX / MapServer.TILE_SIZE);
+        double numPixelY = -(ullat - MapServer.ROOT_ULLAT) / curLatDpp;
+        int startY = (int) Math.floor(numPixelY / MapServer.TILE_SIZE);
+
+        double newUllon = MapServer.ROOT_ULLON + startX * MapServer.TILE_SIZE * curLonDpp;
+        results.put("raster_ul_lon", newUllon);
+
+        double newUllat = MapServer.ROOT_ULLAT + (-1) * startY * MapServer.TILE_SIZE * curLatDpp;
+        results.put("raster_ul_lat", newUllat);
+
+        int nX = 1;
+        while (MapServer.ROOT_ULLON + (startX + nX) * MapServer.TILE_SIZE * curLonDpp < lrlon) {
+            nX += 1;
+        }
+        double newLrlon = MapServer.ROOT_ULLON + (startX + nX) * MapServer.TILE_SIZE * curLonDpp;
+        results.put("raster_lr_lon", newLrlon);
+
+        int nY = 1;
+        while (MapServer.ROOT_ULLAT + (-1) * (startY + nY) * MapServer.TILE_SIZE * curLatDpp > lrlat) {
+            nY += 1;
+        }
+        double newLrlat = MapServer.ROOT_ULLAT + (-1) * (startY + nY) * MapServer.TILE_SIZE * curLatDpp;
+        results.put("raster_lr_lat", newLrlat);
+
+        String[][] render = new String[nY][nX];
+        for (int row = 0; row < nY; ++row) {
+            for (int col = 0; col < nX; ++col) {
+                render[row][col] = String.format("d%d_x%d_y%d.png", depth, startX + col, startY + row);
+            }
+        }
+        results.put("render_grid", render);
+        results.put("query_success", true);
         return results;
     }
 
+    private boolean in(double lon, double lat) {
+        if (lon > MapServer.ROOT_LRLON || lon < MapServer.ROOT_ULLON) {
+            return false;
+        }
+        if (lat < MapServer.ROOT_LRLAT || lat > MapServer.ROOT_ULLAT) {
+            return false;
+        }
+        return true;
+    }
+    private boolean check(double ullon, double ullat, double lrlon, double lrlat) {
+        if (!in(ullon, ullat) && !in(ullon, lrlat) && !in(lrlon, ullat) && !in(lrlon, lrlat)) {
+            return false;
+        }
+        if (ullon > lrlon || ullat < lrlat) {
+            return false;
+        }
+        return true;
+    }
 }
